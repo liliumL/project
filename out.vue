4bc0e0761f07acacd71a5074b490a2d8579c103f
liliumL <lijb>
2019-03-14 11:13:50 +0800

    测试

---
+++ out.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="child">
+ 
+    <h3>这里是子组件</h3>
+    // 作用域插槽
+    <slot :data="data"></slot>
+  </div>
+</template>
+ 
+ export default {
+    data: function(){
+      return {
+        data: ['zhangsan','lisi','wanwu','zhaoliu','tianqi','xiaoba']
+      }
+    }
+}
